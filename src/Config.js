6b0e930ce388f8f4f4a181b0635f89a686013108
andres <a.delabat@alumnos.upm.es>
2020-03-13 14:19:23 +0100

    13/03

--- src/Config.js
+++ src/Config.js
@@ -7,101 +7,71 @@ export default class App extends Component {
 			{ name: "title:", value: this.props.title, type: "text", callback: (e) => {this.props.onConfigChange("title", e.target.value)}},
 			{ name: "showUsername",  noBreak: true, value: this.props.showUsername, type: "checkbox", callback: (e) => {this.props.onConfigChange("showUsername", !this.props.showUsername,)}},
 			{ name: "timeout", friendlyName: "Timeout", value: this.props.timeout, type: "number", callback: (e) => {this.props.onConfigChange("timeout", parseInt(e.target.value))}},
-			{ name: "successMessage:", value: this.props.good, type: "text", callback: (e) => {this.props.onConfigChange("good", e.target.value)}},
-			{ name: "failMessage:", value: this.props.bad, type: "text", callback: (e) => {this.props.onConfigChange("bad", e.target.value)}},
 			{ name: "Using Escapp??", value: this.props.escapp, type: "checkbox", callback: (e) => {this.props.onConfigChange("escapp", !this.props.escapp,)}},
+			{ name: "Theme", value: this.props.theme, type: "select", options:["dark","lux","cerulean","cyborg","pulse","sketchy","superhero"], callback: (e) => {this.props.onConfigChange("theme", e.target.value)}},
+			{ name: "Quest Type", value: this.props.theme, type: "select", options:["Symbol", "AlphaNumeric", "Pattern", "CombinationLock"], callback: (e) => {this.props.onConfigChange("mode", e.target.value)}}
 		];
 
 		let info;
 		let escapp;
 		if(this.props.mode === "CombinationLock"){
 			info= <div>
-					  <label htmlFor="CombinationLockImage"><b>CombinationLockImage:</b></label><p/>
+					  <label htmlFor="CombinationLockImage"><b>Combination Lock Image: </b></label>
 						<input name="CombinationLockImage:" type="file" value={undefined} onChange={(e) => {this.readFile(e.target.files[0], res => this.props.onConfigChange("CombinationLockImage", res),true)}}/>
-						<p/><label htmlFor="answer"><b>CombinationLockAnswer:</b></label><p/>
+						<br/><br/>
+						<label htmlFor="answer"><b>Combination Lock Answer:</b></label>
 					 	<input name="Answer:" type="text" value={this.props.answer} onChange={(e) => {this.props.onConfigChange("answer", e.target.value)}}/>
+					 	<br/>
 						</div>
 		}
 		if(this.props.mode === "Pattern"){
 			info= <div>
-					  <label htmlFor="tip"><b>Tip:</b></label><p/>
+					  <label htmlFor="tip"><b>Tip:</b> </label>
 						<input name="Tip:" type="text" value={this.props.tip} onChange={(e) => {this.props.onConfigChange("tip", e.target.value)}}/>
 						</div>
 		}
 		if(this.props.escapp){
 			escapp= <div><br/>
-						<label htmlFor="puzzleId"><b>PuzzleId:</b></label>&nbsp;
+						<label htmlFor="puzzleId"><b>Puzzle Id:</b></label>&nbsp;
 						<input name="puzzleId:" type="number" value={this.props.puzzleId} onChange={(e) => {this.props.onConfigChange("puzzleId", e.target.value)}}/>&nbsp;
-						<label htmlFor="escapeRoomId"><b>EscapeRoomId:</b></label>&nbsp;
+						<label htmlFor="escapeRoomId"><b>Escape Room Id:</b></label>&nbsp;
 						<input name="escapeRoomId:" type="number" value={this.props.escapeRoomId} onChange={(e) => {this.props.onConfigChange("escapeRoomId", e.target.value)}}/>
-						<p/><label htmlFor="puzzleLength"><b>PuzzleLength:</b></label>&nbsp;
+						<p/><label htmlFor="puzzleLength"><b>Answer Length:</b></label>&nbsp;
 						<input name="puzzleLength:" type="number" value={this.props.puzzleLength} onChange={(e) => {this.props.onConfigChange("puzzleLength", e.target.value)}}/>
 						</div>
 		}else{
 			if(this.props.mode === "Pattern" || "AlphaNumeric" || "Symbol"){
 				escapp= <div>
 				<p/><label htmlFor="answer"><b>Answer:</b></label><p/>
-				<input name="Answer:" type="text" value={this.props.answer} onChange={(e) => {this.props.onConfigChange("answer", e.target.value)}}/></div>
+				<input name="Answer:" type="text" value={this.props.answer} onChange={(e) => {this.props.onConfigChange("answer", e.target.value)}}/>
+				<p/><label htmlFor=""><b>SuccessMessage</b></label><p/>
+				<input name="successMessage:" type="text" value={this.props.good} onChange={(e) => {this.props.onConfigChange("good", e.target.value)}}/>
+				<p/><label htmlFor=""><b>FailMessage</b></label><p/>
+				<input name="FailMessage:" type="text" value={this.props.bad} onChange={(e) => {this.props.onConfigChange("bad", e.target.value)}}/></div>
 			}
 
 		}
-
-
 
 		return <div className="config">
 			{options.map(opt=>{
 				return [<div className="form-group" >
 				<label htmlFor={opt.name}><b>{opt.friendlyName || this.humanize(opt.name)}</b></label>
+				{opt.type == "select" ?
+				<select name={opt.name} onChange={opt.callback}>
+					{opt.options.map(op=><option value={op} selected={op===opt.value}>{this.humanize(op)}</option>)}
+				</select>:
 				<input name={opt.name} type={opt.type} value={opt.type === "file"? undefined: opt.value} min={opt.min} checked={opt.value} onChange={opt.callback}/>
+				}
 			</div>, opt.noBreak ? null: <br/>]
-			})}
 
-			<div className="form-group">
-				<label htmlFor="mode"><b>Quest Type:</b></label><p/>
-				<label htmlFor="mode">{"Symbol"}</label>
-				<input name="mode" type="radio" value={"Symbol"} checked={this.props.mode === "Symbol"} onChange={(e) => {this.props.onConfigChange("mode", "Symbol")}}/>
-				<label htmlFor="mode">{"AlphaNumeric"}</label>
-				<input name="mode" type="radio" value={"AlphaNumeric"} checked={this.props.mode === "AlphaNumeric"} onChange={(e) => {this.props.onConfigChange("mode", "AlphaNumeric")}}/>
-				<p/><label htmlFor="mode">{"Pattern"}</label>
-				<input name="mode" type="radio" value={"Pattern"} checked={this.props.mode === "Pattern"} onChange={(e) => {this.props.onConfigChange("mode", "Pattern")}}/>
-				<label htmlFor="mode">{"CombinationLock"}</label>
-				<input name="mode" type="radio" value={"CombinationLock"} checked={this.props.mode === "CombinationLock"} onChange={(e) => {this.props.onConfigChange("mode", "CombinationLock")}}/>
-			</div>
-			<p/>
+			})}
+			<br/>
 			{info}
 			{escapp}
 
 
-
+			<br/>
 			<div className="form-group">
-				<label htmlFor="theme"><b>Theme:</b></label><p/>
-				<label htmlFor="theme">{"Dark"}</label>
-				<input name="theme" type="radio" value={"dark"} checked={this.props.theme === "dark"} onChange={(e) => {this.props.onConfigChange("theme", "dark")}}/>
-				<label htmlFor="theme">{"Lux"}</label>
-				<input name="theme" type="radio" value={"lux"} checked={this.props.theme === "lux"} onChange={(e) => {this.props.onConfigChange("theme", "lux")}}/>
-				<label htmlFor="theme">{"Cerulean"}</label>
-				<input name="theme" type="radio" value={"cerulean"} checked={this.props.theme === "cerulean"} onChange={(e) => {this.props.onConfigChange("theme", "cerulean")}}/>
-				<label htmlFor="theme">{"Cyborg"}</label>
-				<input name="theme" type="radio" value={"cyborg"} checked={this.props.theme === "cyborg"} onChange={(e) => {this.props.onConfigChange("theme", "cyborg")}}/>
-				<label htmlFor="theme">{"Pulse"}</label>
-				<input name="theme" type="radio" value={"pulse"} checked={this.props.theme === "pulse"} onChange={(e) => {this.props.onConfigChange("theme", "pulse")}}/>
-				<label htmlFor="theme">{"Sketchy"}</label>
-				<input name="theme" type="radio" value={"sketchy"} checked={this.props.theme === "sketchy"} onChange={(e) => {this.props.onConfigChange("theme", "sketchy")}}/>
-				<label htmlFor="theme">{"Superhero"}</label>
-				<input name="theme" type="radio" value={"superhero"} checked={this.props.theme === "superhero"} onChange={(e) => {this.props.onConfigChange("theme", "superhero")}}/>
-
-				<br/><br/>
-				<select name="menu">
-				  <option name="theme" value={"dark"} checked={this.props.theme === "dark"} onChange={(e) => {this.props.onConfigChange("theme", "dark")}}> Dark </option>
-				  <option name="theme" value={"lux"} checked={this.props.theme === "lux"} onChange={(e) => {this.props.onConfigChange("theme", "lux")}}> Lux </option>
-					<option name="theme" value={"cerulean"} checked={this.props.theme === "cerulean"} onChange={(e) => {this.props.onConfigChange("theme", "cerulean")}}> Cerulean </option>
-					<option name="theme" value={"cyborg"} checked={this.props.theme === "cyborg"} onChange={(e) => {this.props.onConfigChange("theme", "cyborg")}}> Cyborg </option>
-					<option name="theme" value={"pulse"} checked={this.props.theme === "pulse"} onChange={(e) => {this.props.onConfigChange("theme", "pulse")}}> Pulse </option>
-					<option name="theme" value={"sketchy"} checked={this.props.theme === "sketchy"} onChange={(e) => {this.props.onConfigChange("theme", "sketchy")}}> Sketchy </option>
-					<option name="theme" value={"superhero"} checked={this.props.theme === "superhero"} onChange={(e) => {this.props.onConfigChange("theme", "superhero")}}> Superhero </option>
-				</select>
-			</div>
-			<div className="form-group"><br/>
 				<label htmlFor="scormVersion"><b>SCORM Version:</b></label><p/>
 				<label htmlFor="scormVersion">{"SCORM 1.2"}</label>
 				<input name="scormVersion" type="radio" value={"1.2"} checked={this.props.scormVersion === "1.2"} onChange={(e) => {this.props.onConfigChange("scormVersion", "1.2")}}/>
--- src/InfoModal.jsx
+++ src/InfoModal.jsx
@@ -9,13 +9,19 @@ export default class InfoModal extends Component {
 				<button className="modal-close" onClick={this.props.hide}>
 				  <i className="material-icons">close</i>
 				</button>
-				<h3>About</h3>
+				<h2>About</h2>
 				<br style={{clear: 'both'}}/>
 				<div className="modal-content">
-				This website allows you to customize your very own quiz application and generate a SCORM package so you can upload it to your preferred LMS.
+				This website allows you to customize your very own lock application and generate a SCORM package so you can upload it to your preferred LMS.
+				<h4>Pattern:</h4>
+					<hr/>
+					This quest type works as a smartphone´s security pattern.
+					Each dot corresponds to a number from 0 to 8 as it can be seen in the picture below:
+					<div><p/><img src="https://raw.githubusercontent.com/WinterCore/react-pattern-lock/HEAD/examples/representation.jpg" width="200px" height="200px" /></div>
 				<h4>Author</h4>
 					<hr/>
 					This project was developed by <a href="http://github.com/adelabat" rel="noopener noreferrer" target="_blank">@adelabat</a> using <a rel="noopener noreferrer" target="_blank" href="https://github.com/agordillo/RESCORM">RESCORM</a>.
+
 					</div>
 				</div>
 			</div>
